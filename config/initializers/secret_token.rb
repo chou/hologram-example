5c918e69dfed22266ce60d3da035930baca93e5c
pivotal <pivotal@pivotal-guest-147.sf.pivotallabs.com>
2014-02-18 11:55:27 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HologramExample::Application.config.secret_key_base = '811c392618280d9cdadd5cf325a5bb6bdff80aea874322f527e370cd1f1ae22beca34b8690800740adcce6b6a5581ccbad88ec05b59bc83a5d7926186750f12f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HologramExample::Application.config.session_store :cookie_store, key: '_hologram-example_session'
